2bf2626507a2602db8bcb9e0b5990fbb68526954
Henry <henry.trowbridge2@gmail.com>
2017-12-15 19:08:48 -0600

    Add files via upload

--- change-youtube-video/script.js
+++ change-youtube-video/script.js
@@ -3,23 +3,42 @@ const inp = document.getElementById("form");
 const base = "https://www.youtube.com/embed/";
 const params = "?autoplay=1&rel=0&modestbranding=1";
 
+// Dual function (no need for 2)...
+function setVid(id) {
+  if (id.length != 11 && id == inp.value) {
+    out.src = base + "4fWyzwo1xg0" + params;
+    alert("Your input must have exactly 11 characters!");
+  } 
+  else if (id.length != 11) {
+    out.src = base + "4fWyzwo1xg0" + params;
+    alert("The developer made a mistake in the code!\nContact him about the issue at henry.trowbridge2@gmail.com.");
+  }
+  else {
+    out.src = base + id + params;
+  }
+}
+
+/* Old functions
+function setVid(id) {
+  if (id.length != 11) {
+    out.src = base + "4fWyzwo1xg0" + params;
+    alert("The developer made a mistake in the code!\nContact him about the issue at henry.trowbridge2@gmail.com.");
+  } 
+  else {
+    out.src = base + id + params;
+  }
+}
+
 function changeVid() {
   if (inp.value.length != 11) {
     out.src = base + "4fWyzwo1xg0" + params;
     alert("Your input must have exactly 11 characters!");
-  } else {
+  } 
+  else {
     out.src = base + inp.value + params;
   }
-}
-
-function setVid(id) {
-  if (id.length != 11) {
-    out.src = base + "4fWyzwo1xg0" + params;
-    alert("The developer made a mistake in the code!\nEmail him at henry.trowbridge2@gmail.com.");
-  } else {
-    out.src = base + id + params;
-  }
-}
+} 
+*/
 
 function showVidId() {
   document.getElementById("showtutorial").style.display = "none";
