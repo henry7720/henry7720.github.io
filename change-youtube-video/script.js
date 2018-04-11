789f44a1ade4787dccd1e3550b4dc080ae36c68c
Henry <19752455+henry7720@users.noreply.github.com>
2018-04-11 18:46:55 -0500

    Update script.js

--- change-youtube-video/script.js
+++ change-youtube-video/script.js
@@ -1,9 +1,10 @@
-const out = document.getElementById("vid");
-const inp = document.getElementById("form");
 const base = "https://www.youtube.com/embed/";
 const params = "?autoplay=1&rel=0&modestbranding=1";
 
-function getYouTubeVideoId(url) {
+var outputVideo = document.getElementById("youtube-player");
+var inputForm = document.getElementById("input-form");
+
+function getVideoId(url) {
   try { var url = new URL(url); }
   catch (err) { return false; }
   if (url.hostname == "www.youtube.com" || url.hostname == "youtube.com") {
@@ -14,35 +15,13 @@ function getYouTubeVideoId(url) {
   return false;
 }
 
-function setVid(youTubeUrlOrId) {
-  if (!getYouTubeVideoId(youTubeUrlOrId)) {
-    if (youTubeUrlOrId.length == 11) out.src = base + youTubeUrlOrId + params;
+function setVid(id) {
+  if (!getVideoId(id)) {
+    if (id.length == 11) out.src = base + youTubeUrlOrId + params;
     else {
       alert("Your input is not a valid YouTube video URL or ID!");
       out.src = base + "4fWyzwo1xg0" + params;
     }
   }
-  else out.src = base + getYouTubeVideoId(youTubeUrlOrId) + params;
+  else out.src = base + getVideoId(id) + params;
 }
-
-/* Old functions
-function setVid(id) {
-  if (id.length != 11) {
-    out.src = base + "4fWyzwo1xg0" + params;
-    alert("The developer made a mistake in the code!\nContact him about the issue at henry.trowbridge2@gmail.com.");
-  } 
-  else {
-    out.src = base + id + params;
-  }
-}
-
-function changeVid() {
-  if (inp.value.length != 11) {
-    out.src = base + "4fWyzwo1xg0" + params;
-    alert("Your input must have exactly 11 characters!");
-  } 
-  else {
-    out.src = base + inp.value + params;
-  }
-} 
-*/
