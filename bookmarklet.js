3836d9fd6a8640a72e30eac4193945db2001ec6f
Henry Trowbridge <Henry@henry.local>
2018-09-08 22:21:47 -0500

    Created bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+var x=document.createElement("iframe");x.src="https://www.linearequations.cf/games/";x.style.cssText="width:100%;height:100vh;position:absolute;z-index:2000;margin:0;padding:0;overflow:hidden;border:none;display:block";x.setAttribute("allowfullscreen","");document.body.appendChild(x)
