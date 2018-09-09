ad8c1679e64d3d65db8a1ced56f6d17b026807ad
Henry <19752455+henry7720@users.noreply.github.com>
2018-09-09 18:18:50 -0500

    Rename embed-bookmarklet.js to embed-bookmarklet.min.js

--- js/embed-bookmarklet.js
+++
@@ -1,1 +0,0 @@
-var e=document.createElement("iframe");e.src="https://www.linearequations.cf/games/";e.style.cssText="width:100%;height:100vh;position:absolute;z-index:2000;margin:0;padding:0;overflow:hidden;border:none;display:block;";e.setAttribute("allowfullscreen","");document.body.appendChild(e);
