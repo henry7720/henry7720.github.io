c244271f1bcdde3b8315723c52b35deff61466bd
Henry <19752455+henry7720@users.noreply.github.com>
2018-09-09 13:51:17 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-var x=document.createElement("iframe");x.src="https://www.linearequations.cf/games/";x.style.cssText="width:100%;height:100vh;position:absolute;z-index:2000;margin:0;padding:0;overflow:hidden;border:none;display:block";x.setAttribute("allowfullscreen","");document.body.appendChild(x)
+var y=document.createElement("iframe");y.src="https://www.linearequations.cf/games/";y.style.cssText="width:100%;height:100vh;position:absolute;z-index:2000;margin:0;padding:0;overflow:hidden;border:none;display:block;";y.setAttribute("allowfullscreen","");document.body.appendChild(y);
